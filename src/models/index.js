98a63dc3d8ab0d1ddc56719be0b40a951a66f714
JavierAzofeifa <javiervisualstudiocode@gmail.com>
2023-11-18 20:07:53 -0600

    first commit

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const Actor = require("./Actor");
+const Director = require("./Director");
+const Genre = require("./Genre");
+const Movie = require("./Movie");
+
+Actor.belongsToMany(Movie, {through: "ActorsMovies"});
+Movie.belongsToMany(Actor, {through: "ActorsMovies"});
+
+Director.belongsToMany(Movie, {through: "DirectorsMovies"});
+Movie.belongsToMany(Director, {through: "DirectorsMovies"});
+
+Movie.belongsToMany(Genre, {through: "MoviesGenres"});
+Genre.belongsToMany(Movie, {through: "MoviesGenres"});
